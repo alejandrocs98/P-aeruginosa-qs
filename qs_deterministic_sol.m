60667795c2cfe91cd1d96ea3383f5898d93ee7fe
Alejandro Castellanos <a.castellanoss@uniandes.edu.co>
2022-02-17 19:36:45 -0500

    New full deterministic model

--- qs_deterministic_sol.m
+++ qs_deterministic_sol.m
@@ -1,4 +1,4 @@
-global b k_lasR g_lasR k_LasR g_LasR a_lasI b_lasI K1 h1 g_lasI k_LasI g_LasI k_AI1 g_AI1 k_LasRAI1 g_LasRAI1 s_LasRAI1
+global b k_lasR g_lasR k_LasR g_LasR a_lasI b_lasI K1 h1 g_lasI k_LasI g_LasI k_AI1 g_AI1 k_LasRAI1 g_LasRAI1 s_LasRAI1 d N d_away g_AI1_ext a_p b_p g_p K2 h2 k_P g_P
 b = 20;
 k_lasR = 200/b;
 g_lasR = 0.347;
@@ -16,8 +16,19 @@ g_AI1 = 0.001;
 k_LasRAI1 = 3;
 g_LasRAI1 = 0.002;
 s_LasRAI1 = 0.1;
-[t,x]=ode45(@qs_deterministic,[0 120],[0 0 0 0 0 0]);
+d = 0;
+N = 0;
+d_away = 0;
+g_AI1_ext = 0;
+a_p = 0;
+b_p = 0;
+K2 = 0;
+h2 = 0;
+g_p = 0;
+k_P = 0;
+g_P = 0;
+[t,x]=ode45(@qs_deterministic,[0 120],[0 0 0 0 0 0 0 0 0]);
 plot(t,x)
 xlabel('Time')
 ylabel('Concentration')
-legend('lasR', 'LasR', 'lasI', 'LasI', 'AI1', 'LasRAI1')
+legend('lasR', 'LasR', 'lasI', 'LasI', 'AI1', 'AI1_ext', 'LasRAI1', 'p', 'P')
