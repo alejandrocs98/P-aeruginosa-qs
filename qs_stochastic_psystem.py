907ac765244b5f3ac7421878c3dfb8b1f4a7cd1c
Alejandro Castellanos <a.castellanoss@uniandes.edu.co>
2022-07-04 11:47:20 -0500

    P systems gillespie code optimized

--- qs_stochastic_psystem.py
+++ qs_stochastic_psystem.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from numba import jit
 
 # Default parameters
 #-----------------------------------------------------------------------------------------------------------------------
@@ -12,9 +13,9 @@ N=10
 
 t_max=10000
 
-b_0=np.array([1, 1, 1, 1, 1, 1, 1, 1])
+b_0=np.array([1, 1, 1, 1, 1, 1, 1, 1], dtype=np.int32)
 
-e_0=np.array([0])
+e_0=np.array([0], dtype=np.int32)
 
 b_update = np.array([[1, 0, 0, 0, 0, 0, 0, 0],    # + lasR
                      [-1, 0, 0, 0, 0, 0, 0, 0],   # - lasR
@@ -36,9 +37,9 @@ b_update = np.array([[1, 0, 0, 0, 0, 0, 0, 0],    # + lasR
                      [0, 0, 0, 0, 0, 0, 0, -1],   # - LasRAI1
                      [0, 0, 0, 0, 0, 0, 1, 0],    # + AI1 ; - AI1_ext
                      [0, 0, 0, 0, 0, 0, 0, 0]],   # - AI1_ext
-            dtype=int)
+            dtype=np.int32)
 
-# e_update = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, -1, -1], dtype=int)
+# e_update = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, -1, -1], dtype=np.int32)
 e_update = np.array([[0],   # + lasR
                      [0],   # - lasR
                      [0],   # + LasR
@@ -59,7 +60,7 @@ e_update = np.array([[0],   # + lasR
                      [0],   # - LasRAI1
                      [-1],  # + AI1 ; - AI1_ext
                      [-1]], # - AI1_ext
-            dtype=int)
+            dtype=np.int32)
 
 k_lasR = 0.004    # 0
 g_lasR = 0.002    # 1
@@ -87,13 +88,13 @@ g_LasRAI1 = 0.14  # 22
 d = 0.8           # 23
 
 params_b = np.array([k_lasR, g_lasR, k_LasR, g_LasR, a_rsaL, b_rsaL, K1, h1, g_rsaL, k_RsaL, g_RsaL, a_lasI, \
-                     b_lasI, K2, h2, g_lasI, k_LasI, g_LasI, k_AI1, g_AI1, u_LasRAI1, s_LasRAI1, g_LasRAI1, d])
+                     b_lasI, K2, h2, g_lasI, k_LasI, g_LasI, k_AI1, g_AI1, u_LasRAI1, s_LasRAI1, g_LasRAI1, d], dtype=np.float32)
 
 g_AI1_ext = 0.8  # 0
 d = 0.8          # 1
 d_away = 1.2     # 2
 
-params_e = np.array([g_AI1_ext, d, d_away])
+params_e = np.array([g_AI1_ext, d, d_away], dtype=np.float32)
 
 b_file = 'bacteria.tsv'
 e_file = 'environment.tsv'
@@ -101,10 +102,11 @@ e_file = 'environment.tsv'
 # Functions
 #-----------------------------------------------------------------------------------------------------------------------
 
+@jit
 def b_rules(x, params_b):
     
     lasR, LasR, rsaL, RsaL, lasI, LasI, AI1, LasRAI1 = x
-    propensities = np.empty(18, dtype=float)
+    propensities = np.empty(18, dtype=np.float64)
     
     k_lasR    = params_b[0]
     g_lasR    = params_b[1]
@@ -131,8 +133,8 @@ def b_rules(x, params_b):
     g_LasRAI1 = params_b[22]
     d         = params_b[23]
 
-    LasRAI1_ = (LasRAI1/K1 )**h1   # Activator
-    RsaL_ = (RsaL/K2 )**h2      # Repressor
+    LasRAI1_ = (LasRAI1/K1 )**h1
+    RsaL_ = (RsaL/K2 )**h2
 
     propensities[0]  = k_lasR                                                   # + lasR
     propensities[1]  = lasR*g_lasR                                              # - lasR
@@ -160,20 +162,21 @@ def b_rules(x, params_b):
     s = 0
     while s < len(propensities):
         if U < propensities[:s+1].sum()/Stot:
-            return np.array((Tau, s), dtype=[('tau','f4'), ('j','i4')])
+            return Tau, s
         s += 1
 
+@jit
 def e_rules(x, params_e):
     
-    AI1_ext = x
-    propensities = np.empty(2, dtype=float)
+    AI1_ext = x[0]
+    propensities = np.empty(2, dtype=np.float64)
     
     g_AI1_ext   = params_e[0]
     d           = params_e[1]
     d_away      = params_e[2]
     
     if AI1_ext == 0:                        
-        return np.array((0, 99), dtype=[('tau','f4'), ('j','i4')])  # no reaction
+        return 0, 99  # no reaction
     else:
         propensities[0] = AI1_ext*d                     # + AI1 ; - AI1_ext
         propensities[1] = AI1_ext*(d_away+g_AI1_ext)    # - AI1_ext
@@ -185,10 +188,11 @@ def e_rules(x, params_e):
         s = 0
         while s < len(propensities):
             if U < propensities[:s+1].sum()/Stot:
-                return np.array((Tau, 18+s), dtype=[('tau','f4'), ('j','i4')])
+                return Tau, 18+s
             s += 1
 
 def save_bacteria(i, t, bacteria, b_file):
+    
     with open(b_file, 'a') as bacteria_file:
         bacteria_file.write(f'{i}\t')
         bacteria_file.write(f'{t}\t')
@@ -202,6 +206,7 @@ def save_bacteria(i, t, bacteria, b_file):
         bacteria_file.write(f'\n')
 
 def save_environment(t, env, e_file):
+
     with open(e_file, 'a') as env_file:
         env_file.write(f'{t}\t')
         x = 0
@@ -213,11 +218,13 @@ def save_environment(t, env, e_file):
             x += 1
         env_file.write(f'\n')
 
+@jit
 def waiting_time(reactions_sorted, h):
+    
     if h > 0:
-        return reactions_sorted[h]['tau'] - reactions_sorted[h-1]['tau']
+        return reactions_sorted[h,0] - reactions_sorted[h-1,0]
     else:
-        return reactions_sorted[h]['tau']
+        return reactions_sorted[h,0]
 
 def multicompartmental_gillespie(N=N, t_max=t_max, b_0=b_0, e_0=e_0, b_update=b_update, e_update=e_update, \
     params_b=params_b, params_e=params_e, b_file=b_file, e_file=e_file):
@@ -231,7 +238,7 @@ def multicompartmental_gillespie(N=N, t_max=t_max, b_0=b_0, e_0=e_0, b_update=b_
     t = 0
 
     # Simulation initialization
-    bacteria = np.zeros([N, len(b_0)])
+    bacteria = np.zeros((N, len(b_0)), dtype=np.int64)
     i = 0
     while i < N:
         x = 0
@@ -240,80 +247,82 @@ def multicompartmental_gillespie(N=N, t_max=t_max, b_0=b_0, e_0=e_0, b_update=b_
             x += 1
         i += 1
     bacteria_file = pd.DataFrame(bacteria, columns=['lasR', 'LasR', 'rsaL', 'RsaL', 'lasI', 'LasI', 'AI1', 'LasRAI1'])
-    bacteria_file.insert(0, 't', np.zeros(N))
-    bacteria_file.insert(0, 'cell', np.array(range(N)))
+    bacteria_file.insert(0, 't', np.zeros(N, dtype=np.int32))
+    bacteria_file.insert(0, 'cell', np.array(range(N), dtype=np.int64))
     bacteria_file.set_index('cell', inplace=True)
     bacteria_file.to_csv(b_file, sep='\t')
     del bacteria_file
 
-    env = np.zeros(len(e_0))
+    env = np.zeros(len(e_0), dtype=np.int64)
     x = 0
     while x < len(e_0):
         env[x] = e_0[x]
         x += 1
     env_file = pd.DataFrame(env, columns=['AI1_ext'])
-    env_file.insert(0, 't', np.zeros(1))
+    env_file.insert(0, 't', np.zeros(1, dtype=np.int32))
     env_file.set_index('t', inplace=True)
     env_file.to_csv(e_file, sep='\t')
     del env_file
     
     # Simulation
     while t < t_max:
-        reactions = np.zeros(N+1, dtype=[('tau','f4'), ('j','i4'), ('i','i4')])
+        reactions = np.zeros((N+1, 3), dtype=np.float64)  # Tau, j, i
         i = 0
         while i < N:
-            reactions[i]['tau'] = b_rules(bacteria[i], params_b)['tau']
-            reactions[i]['j'] = b_rules(bacteria[i], params_b)['j']
-            reactions[i]['i'] = i
+            reactions[i][0] = b_rules(bacteria[i], params_b)[0] # Tau
+            reactions[i][1] = b_rules(bacteria[i], params_b)[1] # j
+            reactions[i][2] = i                                 # i
             i += 1
-        reactions[N]['tau'] = e_rules(env, params_e)['tau']
-        reactions[N]['j'] = e_rules(env, params_e)['j']
-        reactions[N]['i'] = N
+        reactions[N][0] = e_rules(env, params_e)[0] # Tau
+        reactions[N][1] = e_rules(env, params_e)[1] # j
+        reactions[N][2] = N                         # i
         
-        reactions_sorted = np.sort(reactions, order='tau')
+        reactions_sorted = reactions[reactions[:,0].argsort()]
 
         h = 0
         while h < N+1:
-            if reactions_sorted[h]['i'] == N:
-                if reactions_sorted[h]['j'] == 99:
+            compartment = int(reactions_sorted[h,2])
+            reaction = int(reactions_sorted[h,1])
+            if compartment == N:
+                if reaction == 99:
                     h += 1
                     continue
                 else:
                     t += waiting_time(reactions_sorted, h)
-                    reactions_sorted[h]['tau'] = 0
-                    env += e_update[reactions_sorted[h]['j']]
+                    reactions_sorted[h,0] = 0
+                    env += e_update[reaction]
                     save_environment(t, env, e_file)
-                    if reactions_sorted[h]['j'] == 18:
+                    if reaction == 18:
                         i = np.random.randint(0,N)
-                        bacteria[i] += b_update[reactions_sorted[h]['j']]
+                        bacteria[i] += b_update[reaction]
                         save_bacteria(i, t, bacteria, b_file)
                         # Replacement
                         y = 0
                         while y < N+1:
-                            if reactions_sorted[y]['i'] == i:
+                            if reactions_sorted[y,2] == i:
                                 if y > h:
-                                    reactions_sorted[y]['tau'] = b_rules(bacteria[i], params_b)['tau']
-                                    reactions_sorted[y]['j'] = b_rules(bacteria[i], params_b)['j']
-                                    reactions_sorted = np.sort(reactions_sorted, order='tau')
+                                    reactions_sorted[y,0] = b_rules(bacteria[i], params_b)[0]
+                                    reactions_sorted[y,1] = b_rules(bacteria[i], params_b)[1]
+                                    reactions_sorted = reactions_sorted[reactions_sorted[:,0].argsort()]
                             y += 1
             
             else:
                 t += waiting_time(reactions_sorted, h)
-                reactions_sorted[h]['tau'] = 0
-                i = reactions_sorted[h]['i']
-                bacteria[i] += b_update[reactions_sorted[h]['j']]
+                reactions_sorted[h,0] = 0
+                i = compartment
+                bacteria[i] += b_update[reaction]
                 save_bacteria(i, t, bacteria, b_file)
-                if reactions_sorted[h]['j'] == 16:
-                    env += e_update[reactions_sorted[h]['j']]
+                if reaction == 16:
+                    env += e_update[reaction]
                     save_environment(t, env, e_file)
                     # Replacement
                     y = 0
                     while y < N+1:
-                        if reactions_sorted[y]['i'] == N:
+                        if reactions_sorted[y,2] == N:
                             if y > h:
-                                reactions_sorted[y]['tau'] = e_rules(env, params_e)['tau']
-                                reactions_sorted[y]['j'] = e_rules(env, params_e)['j']
-                                reactions_sorted = np.sort(reactions_sorted, order='tau')
+                                reactions_sorted[y,0] = e_rules(env, params_e)[0]
+                                reactions_sorted[y,1] = e_rules(env, params_e)[1]
+                                reactions_sorted = reactions_sorted[reactions_sorted[:,0].argsort()]
                         y += 1
                         
             h += 1
